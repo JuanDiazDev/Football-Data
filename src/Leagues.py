7140ab66733bc2ca32eb74cc2984d01d5101d6bc
Juan Diaz <juanmanuel32@ciencias.unam.mx>
2020-11-08 23:20:39 -0700

    Update Leagues.py

--- src/Leagues.py
+++ src/Leagues.py
@@ -12,23 +12,23 @@ class Leagues:
         'x-rapidapi-host': "api-football-v1.p.rapidapi.com"
     }
 
-    def get_leagues(self):
+    def response_json(self):
         league_url = Leagues.url + f"{self.league}/2020"
         response = requests.request("GET", league_url, headers=Leagues.headers)
-        response_json = response.json()
+        return response.json()
+
+    def get_leagues(self):
+        response = self.response_json()
         leagues = {}
-        for i in response_json["api"]["leagues"]:
+        for i in response["api"]["leagues"]:
             if i["type"] == "League":
                 leagues[i["name"]] = i["league_id"]
         return leagues
 
     def get_cups(self):
-        league_url = Leagues.url + f"{self.league}/2020"
-        response = requests.request("GET", league_url, headers=Leagues.headers)
-        response_json = response.json()
+        response = self.response_json()
         cups = {}
-        for i in response_json["api"]["leagues"]:
+        for i in response["api"]["leagues"]:
             if i["type"] == "Cup":
                 cups[i["name"]] = i["league_id"]
         return cups
-
